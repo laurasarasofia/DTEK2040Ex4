a8b2a9a1cb8efb76fa7853af4ac3c9df717a80d3
laurasarasofia <laurasarasofiaa@gmail.com>
2022-04-26 22:53:48 +0300

    4.7 valmis
    muutettu muistiinpanot suomeksi
    luotu stacknavigator, jotta ohjelmassa lisätään muistiinpano eri tilassa kuin päänäytössä

--- notes/App.js
+++ notes/App.js
@@ -2,6 +2,7 @@ import { StyleSheet, Button, SafeAreaView, TextInput, Text, ScrollView, Alert } 
 import { StatusBar } from "expo-status-bar";
 import React from 'react';
 import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
 const Stack = createStackNavigator();
 
@@ -11,34 +12,29 @@ class App extends React.Component {
     super(props)
     this.state = {
       notes: [
-        "Note1",
-        "Note2",
-        "Note3",
-        "Note4",
-        "Note2",
-        "Note3",
-        "Note4",
-        "Note2",
-        "Note3",
-        "Note4",
-        "Note2",
-        "Note3",
-        "Note4",
-        "Note4",
-        "Note2",
-        "Note3",
-        "Note4",
-        "Note4",
-        "Note2",
-        "Note3",
-        "Note4"
+        "Muistiinpano1",
+        "Muistiinpano2",
+        "Muistiinpano3",
+        "Muistiinpano4",
+        "Muistiinpano5",
+        "Muistiinpano6",
+        "Muistiinpano7",
+        "Muistiinpano8",
+        "Muistiinpano9",
+        "Muistiinpano10",
+        "Muistiinpano11",
+        "Muistiinpano12",
+        "Muistiinpano13",
+        "Muistiinpano14",
+        "Muistiinpano15"
+        
       ],
       text: "",
       setText: ""
     }
 
   }
-  createAlert = () =>{
+  createAlert = () => {
     Alert.alert(
       "Added note already exists!",
       "Please write a new note.",
@@ -72,25 +68,33 @@ class App extends React.Component {
 
   }
 
-  
 
-
-  render() {
-
+  DefaultScreen = (props) => {
     return (
       <SafeAreaView style={styles.container}>
-        <Text style={styles.title}>Muistiinpanot:</Text>
         <ScrollView style={styles.contentContainer}>
           {this.state.notes.map(note => <Text style={styles.notes}>{note}</Text>)}
+
         </ScrollView>
+        <Button
+          title="Lisää muistiinpano"
+          color='#e8bd48'
+          style={styles.button}
+          onPress={() => props.navigation.navigate("Lisää muistiinpano: ")}
+        />
+      </SafeAreaView>
+    )
+  }
+
+  AddScreen = (props) => {
+    return (
+      <SafeAreaView>
         <TextInput
           placeholder="muistiinpano"
 
           onChangeText={(newText) => this.setState({ setText: newText })}
           keyboardType="default"
           style={styles.input}
-          
-
         />
         <Button
           title="Lisää muistiinpano"
@@ -98,7 +102,22 @@ class App extends React.Component {
           onPress={this.addNote}
         />
 
-      </SafeAreaView >
+      </SafeAreaView>
+    )
+  }
+
+  render() {
+
+    return (
+
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Muistikirja: ">
+          <Stack.Screen name="Muistikirja: " component={this.DefaultScreen} />
+          <Stack.Screen name="Lisää muistiinpano: " component={this.AddScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+
+
     );
   }
 }
@@ -120,7 +139,7 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     textAlign: "center",
-    flex:2,
+    flex: 2,
     backgroundColor: "#ffe0f5",
 
 
@@ -132,11 +151,15 @@ const styles = StyleSheet.create({
     fontSize: 30,
     textAlign: "center"
   },
-  notes:{
-    color:"#ffffff",
+  notes: {
+    color: "#ffffff",
     fontWeight: "bold",
     fontSize: 25
 
+  },
+  button: {
+    fontSize:30,
+    fontWeight: "bold"
   }
 });
 
